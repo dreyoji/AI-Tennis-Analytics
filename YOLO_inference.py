c18e96c637e9e737249b154d2b1caacda837939c
dreyoji <90445047+dreyoji@users.noreply.github.com>
2024-04-03 05:42:21 +0900

    Changes for Ball Detection
    + Increased Epochs resulting more accurate

--- YOLO_inference.py
+++ YOLO_inference.py
@@ -2,7 +2,7 @@
 from ultralytics import YOLO
 
 # Load model
-model = YOLO('models/yolov5_best.pt')
+model = YOLO('models/yolov5_last.pt')
 
 # result = model.predict(
 #     source='input/01-screenshot.png',
@@ -12,5 +12,5 @@ model = YOLO('models/yolov5_best.pt')
 result2 = model.predict(
     source='input/01-tennis_video.mp4',
     save=True,
-#    conf=0.2,
+    conf=0.2,
     project='runs/detect')

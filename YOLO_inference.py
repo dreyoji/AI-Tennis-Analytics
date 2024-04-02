abb650288c00469ed5f30da0c8c799d485d63e74
dreyoji <90445047+dreyoji@users.noreply.github.com>
2024-04-02 20:47:05 +0900

    Added Training for Ball Detection

--- YOLO_inference.py
+++ YOLO_inference.py
@@ -1,13 +1,16 @@
+# Import libraries
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+# Load model
+model = YOLO('models/yolov5_best.pt')
 
-result = model.predict(
-    source='input/01-screenshot.png',
-    save=True,
-    project='runs/detect')
+# result = model.predict(
+#     source='input/01-screenshot.png',
+#     save=True,
+#     project='runs/detect')
 
 result2 = model.predict(
     source='input/01-tennis_video.mp4',
     save=True,
+#    conf=0.2,
     project='runs/detect')
